b2772b85e93b4c632ad2f58aac8432c4fe420c30
Jnarhi <redline069@hotmail.com>
2016-02-12 15:55:08 +0200

    perjantai jupailut

---
+++ KertausAppis/Henkilo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KertausAppis
+{
+    class Henkilo
+    {
+        public string Nimi { get; set; }
+        private double paino; // 0 - 100 kg kapselointi!
+        public double Paino
+        {
+            get
+            {
+                return Paino;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100) paino = value;
+                else
+                {
+                    Console.WriteLine("Henkilön paino ei ole sallittu!");
+                    // paino = 0;
+                }
+            }
+        }
+        public Henkilo()
+        {
+        }
+
+        public Henkilo(string nimi, double paino)
+        {
+            Nimi = nimi;
+            Paino = paino;
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " " + paino;
+        }
+    }
+}
---
+++ KertausAppis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KertausAppis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilo henkilo = new Henkilo();
+            henkilo.Nimi = "Jaakko Teppo";
+            henkilo.Paino = 80;
+            Console.WriteLine("Henkilon nimi : " + henkilo.Nimi);
+            Console.WriteLine("Henkilon paino : " + henkilo.Paino);
+
+            string nimi = "Kirsi Kernel";
+            double paino = 50;
+
+            Henkilo henkilo2 = new Henkilo(nimi, paino);
+            Console.WriteLine("Henkilon nimi : " + henkilo2.Nimi);
+            Console.WriteLine("Henkilon paino : " + henkilo2.Paino);
+
+            Henkilo henkilo3 = new Henkilo { Nimi = "Kalle", Paino = 23 };
+
+            Katsoja katsoja = new Katsoja("Minna Teikäläinen", 40, true);
+            Console.WriteLine(katsoja.ToString());
+            katsoja.huutaa();
+        }
+    }
+}
